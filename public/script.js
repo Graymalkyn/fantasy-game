e56aa958af354044dca1e780b4c69c1619979127
Mark Shuler <biggraydog@live.com>
2016-08-16 13:17:26 -0500

    generating random combat numbers

--- public/script.js
+++ public/script.js
@@ -1,12 +1,43 @@
 'use strict';
 if (this.AppName === undefined) this.AppName = {};
 
+
+
 (function(context) {
 
-  function start() {
+  function generateCombatants() {
+    var redNameList = ['red raider', 'red roscoe', 'red ryder', 'ralph'];
+    var blueNameList = ['blue bomber', 'blue blazer', 'blue marlin', 'bob'];
+    var randomRedName = Math.floor(Math.random() * redNameList.length);
+    // console.log(redNameList[randomRedName]);
+    var randomBlueName = Math.floor(Math.random() * blueNameList.length);
+    // console.log(blueNameList[randomBlueName]);
 
+  }
+
+  function getRandom (min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  function generateHp(){
+    var redRandomHp = getRandom(5,10);
+    var blueRandomHp = getRandom(5,10);
+
+    var redGuyHp = redRandomHp;
+    console.log(redGuyHp)
+
+    var blueGuyHp = blueRandomHp;
+    console.log(blueGuyHp)
+
+  }
+
+
+
+  function start() {
+    generateCombatants();
+    generateHp();
     //Call your code here
-    console.log('starting', context);
+    // console.log('starting', context);
 
   }
 
