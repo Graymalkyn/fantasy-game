c36a9a9a50d9834b8ba27e5b370ad8e013d29edd
Mark Shuler <biggraydog@live.com>
2016-08-18 15:16:58 -0500

    end of day push

--- public/character-generator.js
+++ public/character-generator.js
@@ -3,7 +3,7 @@ if (this.FantaSmash === undefined) this.FantaSmash = {};
 (function(context) {
 
   function generateName() {
-    var NameList = ['Ralph', 'Rudolph the Red', 'Kung Fury', 'NEO', 'Daisy Duke', 'Flash Gordon', 'The Dude', 'Bob', 'Bucky the Barbarian', 'Zen Master', 'Agent X', 'Roscoe P. Coaltrain', 'Ming the Merciless', 'The Self Soiler', 'Billy the Bully', 'Ragnarok', 'Valkyrie', 'Party Pooper', 'Underdog', 'Cosmic Cow'];
+    var NameList = ['Ralph', 'Rudolph the Red', 'Kung Fury', 'NEO', 'Daisy Duke', 'Flash Gordon', 'The Dude', 'Bob', 'Bucky the Barbarian', 'Zen Master', 'Agent X', 'Roscoe P. Coaltrain', 'Ming the Merciless', 'The Self Soiler', 'Billy the Bully', 'Ragnarok', 'Valkyrie', 'Party Pooper', 'Underdog', 'Cosmic Cow', 'The Man With No Name', 'Smooth Operator'];
     var randomNameIndex = Math.floor(Math.random() * NameList.length);
     var randomName = NameList[randomNameIndex];
     return randomName;
--- public/script.js
+++ public/script.js
@@ -1,24 +1,50 @@
 'use strict';
 if (this.FantaSmash === undefined) this.FantaSmash = {};
 
+var $startButton = $('.start-button');
+
+  var redGuy;
+  var blueGuy;
+  var templateFunk;
+  var html;
+
+
 (function(context) {
   var $redName = $('.red-dude-all .info');
   var $blueName = $('.blue-dude-all .info');
+  function startTheFight() {
 
+    var damage = Math.floor(Math.random() * 3);
+    redGuy.hp -= damage;
+    html = templateFunk(redGuy);
+    $('.red-dude-all .info').html(html);
+
+    var damage = Math.floor(Math.random() * 3);
+    blueGuy.hp -= damage;
+    html = templateFunk(blueGuy);
+    $('.blue-dude-all .info').html(html);
+
+  }
 
   function start() {
     var templateHtml = $('.character').html();
-    var templateFunk = _.template(templateHtml);
-    var html = templateFunk(context.generateCharacter());
-    $redName.html(html);
+    templateFunk = _.template(templateHtml);
 
-    var templateHtml = $('.character').html();
-    var templateFunk = _.template(templateHtml);
-    var html = templateFunk(context.generateCharacter());
-    $blueName.html(html);
+    redGuy = context.generateCharacter();
+    html = templateFunk(redGuy);
+    $('.red-dude-all .info').html(html);
+
+    blueGuy = context.generateCharacter();
+    html = templateFunk(blueGuy);
+    $('.blue-dude-all .info').html(html);
+
+    $startButton.on('click', startTheFight);
 
   }
 
   context.start = start;
+
+
+
 
 })(window.FantaSmash);
