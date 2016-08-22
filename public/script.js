8ae88f72d28486a935dde0fc8388a3e8af933025
Mark Shuler <biggraydog@live.com>
2016-08-22 14:20:49 -0500

    classroom changes

--- public/script.js
+++ public/script.js
@@ -1,46 +1,106 @@
 'use strict';
 if (this.FantaSmash === undefined) this.FantaSmash = {};
 
-var $startButton = $('.start-button');
 
+
+(function(context) {
+  var $startButton = $('.start-button');
   var redGuy;
   var blueGuy;
   var templateFunk;
   var html;
+  var intervalid;
+  var $message;
 
 
-(function(context) {
-  var $redName = $('.red-dude-all .info');
-  var $blueName = $('.blue-dude-all .info');
-  function startTheFight() {
-
+  function doCombat(){
     var damage = Math.floor(Math.random() * 3);
     redGuy.hp -= damage;
-    html = templateFunk(redGuy);
-    $('.red-dude-all .info').html(html);
+
+    if (redGuy.hp <= 6){
+      $('.red-dude-all .sprite').addClass('red-dude-hurt');
+    }
+    if (redGuy.hp <= 0){
+      $('.red-dude-all .sprite').addClass('red-dude-dead');
+    }
 
     var damage = Math.floor(Math.random() * 3);
     blueGuy.hp -= damage;
+
+    if (blueGuy.hp <= 6){
+      $('.blue-dude-all .sprite').addClass('blue-dude-hurt');
+    }
+    if (blueGuy.hp <= 0){
+      $('.blue-dude-all .sprite').addClass('blue-dude-dead');
+    }
+
+    updateCombatantDisplay();
+
+    if (redGuy.hp <1 || blueGuy.hp <1){
+        endOfBattle();
+    }
+  }
+
+  function endOfBattle(){
+    clearInterval(intervalid);
+    $message.text('Game Over, Man!!')
+
+    $.ajax({
+      url: '/api/leaderboard',
+      method: 'POST',
+      data: {
+      redGuyName: redGuy.name,
+      redguyHP: redGuy.hp,
+      blueGuyName: blueGuy.name,
+      blueGuyHP: blueGuy.hp
+      }
+
+    })
+    .done(function(result){
+      console.log( 'POST done', result);
+    });
+  }
+
+  function startTheFight() {
+    $('.start-button').toggle();
+    intervalid = setInterval(doCombat, 1000);
+
+    $message = $('.message');
+    $message.text('Fighting!!!');
+
+
+
+  }
+
+  function updateCombatantDisplay(){
+
+    var templateHtml = $('.character').html();
+    templateFunk = _.template(templateHtml);
+
+    html = templateFunk(redGuy);
+    $('.red-dude-all .info').html(html);
+
     html = templateFunk(blueGuy);
     $('.blue-dude-all .info').html(html);
+
 
   }
 
   function start() {
-    var templateHtml = $('.character').html();
-    templateFunk = _.template(templateHtml);
 
     redGuy = context.generateCharacter();
-    html = templateFunk(redGuy);
-    $('.red-dude-all .info').html(html);
-
     blueGuy = context.generateCharacter();
-    html = templateFunk(blueGuy);
-    $('.blue-dude-all .info').html(html);
+
+    updateCombatantDisplay();
 
     $startButton.on('click', startTheFight);
 
   }
+
+  // function restart(){
+  //   $startButton.on('click', start());
+  // }
+
 
   context.start = start;
 
--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 var express = require('express'); //grab the express module we downloaded
+var bodyParser = require('body-parser');
 
 var app = express(); //actually create an express app. We'll use this to configure the server.
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 //tells the app to use express' static file functionality and tells it to use the public folder.
 app.use(express.static('public'));
+
+var leaderboardData = [],
+
+app.get('/api/leaderboard', function(req, res){
+  res.send(leaderboardData);
+});
+
+app.post('/api.leaderboard', function(req, res){
+  console.log('POST data', result)
+  leaderboardData.push(req.body);
+  res.send(leaderboardData);
+});
+
+
 
 //gotta listen on a port to accept requests
 app.listen(5500, function() {
